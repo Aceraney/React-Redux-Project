66337e988b07de10235ac9e3787e220048e4bd74
Ace Raney <ace@sfs-wifi-dhcp-10-143-182-140.wifi.sfsu.edu>
2017-02-20 09:02:47 -0800

    changed data dispalyed on each page

--- app/components/SinglePlayer.js
+++ app/components/SinglePlayer.js
@@ -15,9 +15,22 @@ function SinglePlayer (props) {
 	spm = spm.substring(0, spm.indexOf('.'));
 	//remove non significant decimals from the KDR item
 	var kdr = props.object.data.extra.kdr;
-	
 	kdr=kdr.toString();
 	kdr = kdr.substring(0, kdr.indexOf('.')+3);
+
+	//remove non significant decimals from the WLR item
+
+	var wlr = props.object.data.extra.wlr;
+	wlr=wlr.toString();
+	wlr = wlr.substring(0, wlr.indexOf('.')+3);
+
+	//remove non significant decimals from the KPM item
+	var kpm = props.object.data.extra.kpm;
+	kpm=kpm.toString();
+	kpm = kpm.substring(0, kpm.indexOf('.')+3);
+
+
+
 	
 	
 
@@ -29,9 +42,11 @@ function SinglePlayer (props) {
 				</p>
 				<p style={style}>Skill {props.object.data.skill}
 				</p>
-				<p style={style}>Skill: {props.object.data.skill}
+				<p style={style}>Win/Loss: {wlr}
 				</p>
 				<p style={style}>SPM: {spm}
+				</p>
+				<p style={style}>KPM {kpm}
 				</p>
 				<p style={style}>KDR: {kdr}
 				</p>
